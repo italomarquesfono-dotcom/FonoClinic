c4f6587f79aac2b13e50d68b0326832c39fa1a52
italomarquesfono-dotcom <italo.marques.fono@gmail.com>
2026-09-23 22:23:57 -0300

    Add files via upload

---
+++ layout.js
@@ -0,0 +1,3 @@
+import './globals.css'
+export const metadata={title:'FonoClinic',description:'Sistema clínico de Fonoaudiologia'}
+export default function RootLayout({children}){return <html lang="pt-BR"><body>{children}</body></html>}
